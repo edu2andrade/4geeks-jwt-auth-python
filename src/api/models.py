f804594fb53f03f8713934d822edea9705b5c0ac
Eduardo Andrade <edu2andrade@gmail.com>
2023-04-13 13:19:04 +0000

    feat: authentication system with jwt is done

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,20 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(200), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,14 +5,52 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
+import bcrypt
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, get_jwt
+
 api = Blueprint('api', __name__)
 
+@api.route('/users', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    serialized_users = list(map(lambda user: user.serialize(), all_users))
+    return serialized_users
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify(f'User with id: {user_id}, not found in database.'), 404
+    else:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify(f'User with id: {user_id}, is successfully deleted.'), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route('/users/register', methods=['POST'])
+def create_new_user():
+    body = request.get_json()
+    hashed = bcrypt.hashpw(body['password'].encode(), bcrypt.gensalt(14))
+    new_user = User(body['username'], body['email'], hashed.decode())
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 201
 
-    return jsonify(response_body), 200
+@api.route('/users/login', methods = ['POST'])
+def login():
+    body = request.get_json()
+    user = User.query.filter_by(email = body['email']).one()
+
+    if bcrypt.checkpw(body['password'].encode(), user.password.encode()):
+        create_token = create_access_token(identity=user.id)
+        return jsonify(create_token)
+    else:
+        return jsonify('User not exists in database!'), 404
+
+        
+# crear ruta privada!
+@api.route('/users/app', methods = ['GET'])
+@jwt_required()
+def get_logged_user():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    return jsonify({ "id": user.id, "username": user.username, "email": user.email }), 200
